342ce47c93b2493ce8cfa11e7b92e659ac82b941
Carlos Aldama Nalda <caldama@trueblue.com>
2022-03-17 18:09:43 -0600

    Adds first boilerplate code for a simple api get.

---
+++ VinylSpin/VinylSpinService/Controllers/VinylController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VinylSpinService.Controllers
+{
+    [ApiController]
+    [Route("api/Vinyles")]
+    public class VinylController : ControllerBase
+    {
+        [HttpGet]
+        public JsonResult GetVinyles() => new(new List<object> {
+                new { id = 1, Name = "Low" },
+                new { id = 1, Name = "Reality" }
+            });
+    }
+}
